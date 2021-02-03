831635662c7526e60a7bac951bcb4cb5bed8f21d
Josiney Junior <josineyjunior14@gmail.com>
2021-02-03 13:55:25 -0300

    configurando conversor-temperatura.page.html

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  public appPages = [
+    {
+      title: 'Calculadora',
+      url: '../calculadora',
+      icon: 'calculator',
+      color: 'success',
+    },
+    {
+      title: 'Conversor de Temperatura',
+      url: '../conversor-temperatura',
+      icon: 'thermometer',
+      color: 'danger',
+    },
+    {
+      title: 'Calculadora de Idade',
+      url: '../calculadora-idade',
+      icon: 'calendar',
+      color: 'dark',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
