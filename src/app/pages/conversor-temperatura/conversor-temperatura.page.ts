87736c703f20fd561647a68207295d296e4e43c9
Josiney Junior <josineyjunior14@gmail.com>
2021-02-03 13:58:37 -0300

    configurando conversor-temperatura.page.ts

--- src/app/pages/conversor-temperatura/conversor-temperatura.page.ts
+++ src/app/pages/conversor-temperatura/conversor-temperatura.page.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conversor-temperatura.page.scss'],
 })
 export class ConversorTemperaturaPage implements OnInit {
+  escalaDe: string;
+  temperaturaDe: number;
+  escalaPara: string;
+  temperaturaPara: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  converterParaCelcius(tempertura: number): number {
+    return (5 * (tempertura - 32)) / 9;
   }
 
+  converterParaFahrenheit(temperatura: number): number {
+    return (temperatura / 5) * 9 + 32;
+  }
+
+  async converter(): Promise<void> {
+    if (this.escalaPara === 'c') {
+      this.temperaturaPara = this.converterParaCelcius(this.temperaturaDe);
+    } else {
+      if (this.escalaPara === 'f') {
+        this.temperaturaPara = this.converterParaFahrenheit(this.temperaturaDe);
+      }
+    }
+  }
+
+  async limpar(): Promise<void> {
+    this.escalaPara = null;
+    this.escalaDe = null;
+    this.escalaPara = null;
+    this.temperaturaDe = null;
+  }
+
+  ngOnInit() {}
 }
